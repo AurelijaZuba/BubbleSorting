16e31b0759a0ae12badb4b9a9e09465cc5b22f88
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-07-29 12:16:20 +0100

    Passing test for two items

--- src/main/java/com/codurance/BubbleSorting.java
+++ src/main/java/com/codurance/BubbleSorting.java
@@ -3,9 +3,20 @@ package com.codurance;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.util.Arrays.asList;
+
 public class BubbleSorting {
     public List<Integer> sort(List<Integer> objects) {
+        List<Integer> result = new ArrayList<>();
 
-        return objects;
+        if(objects.isEmpty()) {
+            return objects;
+        }
+
+        if(objects.get(0) > objects.get(1)){
+            result = asList(objects.get(1), objects.get(0));
+        }
+
+        return result;
     }
 }
