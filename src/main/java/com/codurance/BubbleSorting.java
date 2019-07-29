180e335ae297d7c2910594a3837aba7f0a7de3c8
AurelijaZuba <auryte00@hotmail.com>
2019-07-29 12:28:37 +0100

    Refactoring extract should swap method

--- src/main/java/com/codurance/BubbleSorting.java
+++ src/main/java/com/codurance/BubbleSorting.java
@@ -3,8 +3,6 @@ package com.codurance;
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.util.Arrays.asList;
-
 public class BubbleSorting {
     public List<Integer> sort(List<Integer> objects) {
         List<Integer> result = new ArrayList<>();
@@ -12,15 +10,20 @@ public class BubbleSorting {
         if(objects.isEmpty()) {
             return objects;
         }
-        if(objects.get(0) > objects.get(1)){
+
+        if(shouldSwap(objects.get(0), objects.get(1))){
             result.add(objects.get(1));
             result.add(objects.get(0));
         }
 
-        if(objects.get(0) > objects.get(1) && objects.size() == 3){
+        if(shouldSwap(objects.get(0), objects.get(1)) && objects.size() == 3){
             result.add(objects.get(2));
         }
 
         return result;
+    }
+
+    private boolean shouldSwap(Integer integerA, Integer integerB) {
+        return integerA > integerB;
     }
 }
