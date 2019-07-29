46b900238e3035c475228eaf9154296cf0009bfd
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-07-29 14:01:11 +0100

    Add failing test for 5 items

--- src/test/java/com/codurance/BubbleSortingShould.java
+++ src/test/java/com/codurance/BubbleSortingShould.java
@@ -27,6 +27,8 @@ public class BubbleSortingShould {
             return Stream.of(Arguments.arguments(asList(), asList()),
                     Arguments.arguments(asList(2, 1), asList(1, 2)),
                     Arguments.arguments(asList(2, 1, 3), asList(1, 2, 3)),
-                    Arguments.arguments(asList(2, 1, 4, 3), asList(1, 2, 3, 4)));
+                    Arguments.arguments(asList(2, 1, 4, 3), asList(1, 2, 3, 4)),
+                    Arguments.arguments(asList(2, 1, 3, 4, 5), asList(1, 2, 3, 4, 5))
+            );
     }
 }
