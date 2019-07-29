66598c549cdb36c7e0d03284cb02937a262154b6
AurelijaZuba <auryte00@hotmail.com>
2019-07-29 12:01:13 +0100

    Failing test for returning empty list

---
+++ src/main/java/com/codurance/BubbleSorting.java
@@ -0,0 +1,10 @@
+package com.codurance;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BubbleSorting {
+    public List<Integer> sort(ArrayList<Object> objects) {
+        return null;
+    }
+}
--- src/test/java/com/codurance/BubbleSortingShould.java
+++ src/test/java/com/codurance/BubbleSortingShould.java
@@ -1,6 +1,22 @@
 package com.codurance;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class BubbleSortingShould {
 
 
+    @Test
+    void return_empty_list() {
+        BubbleSorting bubbleSorting = new BubbleSorting();
+
+        List<Integer> actual = bubbleSorting.sort(new ArrayList<>());
+        List<Integer> expected = new ArrayList<>();
+
+        assertThat(actual).isEqualTo(expected);
+    }
 }
