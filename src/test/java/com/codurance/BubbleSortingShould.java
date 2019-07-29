9e4f0f5c31c67cdf1eb9069e57b6be57f2a29b2c
AurelijaZuba <auryte00@hotmail.com>
2019-07-29 12:06:17 +0100

    Failing test to return sorted list for 2 items

--- src/test/java/com/codurance/BubbleSortingShould.java
+++ src/test/java/com/codurance/BubbleSortingShould.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BubbleSortingShould {
@@ -16,6 +17,16 @@ public class BubbleSortingShould {
 
         List<Integer> actual = bubbleSorting.sort(new ArrayList<>());
         List<Integer> expected = new ArrayList<>();
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void return_sorted_list_for_two_items() {
+        BubbleSorting bubbleSorting = new BubbleSorting();
+
+        List<Integer> actual = bubbleSorting.sort(asList(2, 1));
+        List<Integer> expected = asList(1 ,2);
 
         assertThat(actual).isEqualTo(expected);
     }
